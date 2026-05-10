712f83d04f25c17dc1471db8519fe5e7939a096b
Ori Levi <orile03@gmail.com>
2026-05-11 00:19:03 +0300

    fix tidb ssl connection

--- src/config/db.js
+++ src/config/db.js
@@ -1,15 +1,22 @@
 const mysql = require("mysql2/promise");
 require("dotenv").config();
 
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_PORT:", process.env.DB_PORT);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_NAME:", process.env.DB_NAME);
+
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: Number(process.env.DB_PORT || 4000),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
+  maxIdle: 10,
+  enableKeepAlive: true,
   ssl: {
     minVersion: "TLSv1.2",
     rejectUnauthorized: true,
